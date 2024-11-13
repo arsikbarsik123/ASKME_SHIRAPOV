a037c80e1e4fbb18fd627d88f78f3d68ade38dd7
Arsalan <batyavzdanii749@gmail.com>
2024-11-13 18:07:26 +0300

    last изменение

--- task3/askme_shirapov/asgi.py
+++ task3/askme_shirapov/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for askme_satin project.
+ASGI config for askme_shirapov project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askme_satin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askme_shirapov.settings')
 
 application = get_asgi_application()
